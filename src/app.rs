3faa1339d37004d12213080c7099b30e22d33675
CaymanFreeman <138227022+CaymanFreeman@users.noreply.github.com>
2025-11-11 17:16:54 -0600

    Remove template strings
    Signed-off-by: CaymanFreeman <138227022+CaymanFreeman@users.noreply.github.com>

--- src/app.rs
+++ src/app.rs
@@ -29,7 +29,6 @@ impl Default for App {
 }
 
 impl App {
-    /// Called once before the first frame.
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         let app: Self = if let Some(storage) = cc.storage {
             eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default()
@@ -46,12 +45,10 @@ impl App {
 }
 
 impl eframe::App for App {
-    /// Called each time the UI needs repainting, which may be many times per second.
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         processes::update(self, ctx);
     }
 
-    /// Called by the framework to save state before shutdown.
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
