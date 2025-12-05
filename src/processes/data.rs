6fe10fb373dd5ca78a2c3d3f01e0ef1808a66172
CaymanFreeman <138227022+CaymanFreeman@users.noreply.github.com>
2025-12-04 18:10:47 -0600

    Separation of duties
    Signed-off-by: CaymanFreeman <138227022+CaymanFreeman@users.noreply.github.com>

--- src/processes/data.rs
+++ src/processes/data.rs
@@ -1,9 +1,11 @@
+use std::sync::{Arc, Mutex};
+
 const UNKNOWN_PROCESS_PATH: &str = "-";
 const UNKNOWN_PROCESS_NAME: &str = "-";
 const UNKNOWN_USER: &str = "-";
 
 pub struct ProcessInfo {
-    pub id: String,
+    pub id: u32,
     pub name: String,
     pub user: String,
     pub memory: String,
@@ -14,43 +16,58 @@ pub struct ProcessInfo {
     pub status: String,
 }
 
-fn prepare_info(
+pub fn prepare_processes(system: &Arc<Mutex<sysinfo::System>>) -> Vec<ProcessInfo> {
+    if let Ok(system) = system.lock() {
+        let processes: Vec<&sysinfo::Process> = system.processes().values().collect();
+        let users = sysinfo::Users::new_with_refreshed_list();
+        let cpu_count = system.cpus().len();
+        let processes_info: Vec<ProcessInfo> = processes
+            .iter()
+            .map(|process| extract_info(process, &users, cpu_count))
+            .collect();
+        processes_info
+    } else {
+        Vec::new()
+    }
+}
+
+fn extract_info(
     process: &sysinfo::Process,
     users: &sysinfo::Users,
     cpu_count: usize,
 ) -> ProcessInfo {
     ProcessInfo {
         id: extract_id(process),
-        name: extract_name(process),
-        user: extract_user(process, users),
+        name: extract_name(process)
+            .unwrap_or(UNKNOWN_PROCESS_NAME)
+            .to_owned(),
+        user: extract_user(process, users)
+            .unwrap_or(UNKNOWN_USER)
+            .to_owned(),
         memory: extract_memory(process),
         cpu: extract_cpu(process, cpu_count),
         disk_read: extract_disk_read(process),
         disk_write: extract_disk_write(process),
-        path: extract_path(process),
+        path: extract_path(process)
+            .unwrap_or(UNKNOWN_PROCESS_PATH)
+            .to_owned(),
         status: extract_status(process),
     }
 }
 
-fn extract_id(process: &sysinfo::Process) -> String {
-    process.pid().to_string()
+fn extract_id(process: &sysinfo::Process) -> u32 {
+    process.pid().as_u32()
 }
 
-fn extract_name(process: &sysinfo::Process) -> String {
-    process
-        .name()
-        .to_str()
-        .unwrap_or(UNKNOWN_PROCESS_NAME)
-        .to_owned()
+pub fn extract_name(process: &sysinfo::Process) -> Option<&str> {
+    process.name().to_str()
 }
 
-fn extract_user(process: &sysinfo::Process, users: &sysinfo::Users) -> String {
+fn extract_user<'a>(process: &sysinfo::Process, users: &'a sysinfo::Users) -> Option<&'a str> {
     process
         .user_id()
         .and_then(|uid| users.get_user_by_id(uid))
         .map(|user| user.name())
-        .unwrap_or(UNKNOWN_USER)
-        .to_owned()
 }
 
 fn extract_memory(process: &sysinfo::Process) -> String {
@@ -69,12 +86,8 @@ fn extract_disk_write(process: &sysinfo::Process) -> String {
     bytesize::ByteSize(process.disk_usage().written_bytes).to_string()
 }
 
-fn extract_path(process: &sysinfo::Process) -> String {
-    process
-        .exe()
-        .and_then(|path| path.to_str())
-        .unwrap_or(UNKNOWN_PROCESS_PATH)
-        .to_owned()
+pub fn extract_path(process: &sysinfo::Process) -> Option<&str> {
+    process.exe().and_then(|path| path.to_str())
 }
 
 fn extract_status(process: &sysinfo::Process) -> String {
---
+++ src/processes/ui.rs
@@ -0,0 +1,136 @@
+use crate::app;
+use crate::processes::data;
+use std::ops::RangeInclusive;
+
+const HEADER_TEXT_SIZE: f32 = 12.0;
+const HEADER_HEIGHT: f32 = 25.0;
+const ROW_HEIGHT: f32 = 18.0;
+
+const CONTROL_PANEL_MIN_HEIGHT: f32 = 40.0;
+const CONTROL_BUTTON_TEXT_SIZE: f32 = 16.0;
+const CONTROL_BUTTON_SIZE: [f32; 2] = [50.0, 25.0];
+
+const COLUMN_WIDTH_RANGE: RangeInclusive<f32> = 75.0..=500.0;
+const BLANK_PROCESS_PATH: &str = "";
+const BLANK_PROCESS_NAME: &str = "";
+
+pub fn update(app: &mut app::App, ctx: &egui::Context) {
+    egui::TopBottomPanel::top("options_bar").show(ctx, |ui| {
+        update_options_panel(app, ui);
+    });
+    egui::TopBottomPanel::bottom("control_bar")
+        .min_height(CONTROL_PANEL_MIN_HEIGHT)
+        .show(ctx, |ui| {
+            update_control_bar(app, ctx, ui);
+        });
+    egui::CentralPanel::default().show(ctx, |ui| {
+        update_table(app, ui);
+    });
+}
+
+fn update_options_panel(app: &mut app::App, ui: &mut egui::Ui) {
+    ui.horizontal(|ui| {
+        ui.add(egui::TextEdit::singleline(&mut app.process_filter).hint_text("Filter processes"));
+        ui.checkbox(&mut app.show_thread_processes, "Threads");
+    });
+}
+
+fn control_button(text: impl Into<String>, ui: &mut egui::Ui) -> egui::Response {
+    ui.add(
+        egui::Button::new(
+            egui::RichText::new(text).font(egui::FontId::proportional(CONTROL_BUTTON_TEXT_SIZE)),
+        )
+        .min_size(egui::Vec2::from(CONTROL_BUTTON_SIZE)),
+    )
+}
+
+#[expect(clippy::collapsible_if)]
+fn update_control_bar(app: &app::App, ctx: &egui::Context, ui: &mut egui::Ui) {
+    if let (Some(pid), Ok(system)) = (app.selected_pid, app.system.lock()) {
+        if let Some(process) = system.processes().get(&sysinfo::Pid::from_u32(pid)) {
+            ui.horizontal_centered(|ui| {
+                if control_button("Terminate", ui).clicked() {
+                    process.kill_with(sysinfo::Signal::Term);
+                }
+
+                if control_button("Kill", ui).clicked() {
+                    process.kill_with(sysinfo::Signal::Kill);
+                }
+
+                if control_button("Copy Path", ui).clicked() {
+                    ctx.copy_text(
+                        data::extract_path(process)
+                            .unwrap_or(BLANK_PROCESS_PATH)
+                            .to_owned(),
+                    );
+                }
+
+                if control_button("Copy Name", ui).clicked() {
+                    ctx.copy_text(
+                        data::extract_name(process)
+                            .unwrap_or(BLANK_PROCESS_NAME)
+                            .to_owned(),
+                    );
+                }
+
+                if control_button("Copy PID", ui).clicked() {
+                    ctx.copy_text(process.pid().to_string());
+                }
+            });
+        }
+    }
+}
+
+fn header_label(text: impl Into<String>, ui: &mut egui::Ui) {
+    ui.label(egui::RichText::new(text).font(egui::FontId::proportional(HEADER_TEXT_SIZE)));
+}
+
+fn cell_label(text: impl Into<egui::WidgetText>, ui: &mut egui::Ui) {
+    ui.style_mut().interaction.selectable_labels = false;
+    ui.add(egui::Label::new(text).truncate());
+}
+
+fn update_table(app: &mut app::App, ui: &mut egui::Ui) {
+    let processes_info = data::prepare_processes(&app.system);
+    egui_extras::TableBuilder::new(ui)
+        .striped(true)
+        .columns(
+            egui_extras::Column::auto_with_initial_suggestion(*COLUMN_WIDTH_RANGE.start())
+                .range(COLUMN_WIDTH_RANGE)
+                .resizable(true),
+            9,
+        )
+        .header(HEADER_HEIGHT, |mut header_row| {
+            header_row.col(|ui| header_label("Name", ui));
+            header_row.col(|ui| header_label("ID", ui));
+            header_row.col(|ui| header_label("User", ui));
+            header_row.col(|ui| header_label("Memory", ui));
+            header_row.col(|ui| header_label("CPU", ui));
+            header_row.col(|ui| header_label("Disk Read", ui));
+            header_row.col(|ui| header_label("Disk Write", ui));
+            header_row.col(|ui| header_label("Path", ui));
+            header_row.col(|ui| header_label("Status", ui));
+        })
+        .body(|mut body_rows| {
+            for process_info in &processes_info {
+                body_rows.row(ROW_HEIGHT, |mut row| {
+                    row.set_selected(app.selected_pid == Some(process_info.id));
+
+                    row.col(|ui| cell_label(&process_info.name, ui));
+                    row.col(|ui| cell_label(process_info.id.to_string(), ui));
+                    row.col(|ui| cell_label(&process_info.user, ui));
+                    row.col(|ui| cell_label(&process_info.memory, ui));
+                    row.col(|ui| cell_label(&process_info.cpu, ui));
+                    row.col(|ui| cell_label(&process_info.disk_read, ui));
+                    row.col(|ui| cell_label(&process_info.disk_write, ui));
+                    row.col(|ui| cell_label(&process_info.path, ui));
+                    row.col(|ui| cell_label(&process_info.status, ui));
+
+                    let response = row.response();
+                    if response.hovered() && response.ctx.input(|i| i.pointer.primary_clicked()) {
+                        app.selected_pid = Some(process_info.id);
+                    }
+                });
+            }
+        });
+}
